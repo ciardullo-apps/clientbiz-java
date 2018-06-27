00972fd0cca5cde7cb400534f0969096435b155a
John Ciardullo <ciardullo.apps@gmail.com>
2018-06-26 20:31:58 -0400

    Parameterize DB configuration

--- src/main/java/org/ciardullo/config/DbConfig.java
+++ src/main/java/org/ciardullo/config/DbConfig.java
@@ -4,27 +4,41 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
 @MapperScan("org.ciardullo.data.mapper")
+@PropertySource("classpath:config/db.properties")
 public class DbConfig {
+    @Autowired
+    Environment env;
+
     @Bean
     public DataSource dataSource() {
-        // TODO Parameterize credentials
         DriverManagerDataSource dataSource =
-                new DriverManagerDataSource("jdbc:mysql://localhost:3306/clientbiz?useUnicode=yes&characterEncoding=UTF-8", "john", "test");
+                new DriverManagerDataSource(env.getProperty("db.url"),
+                        env.getProperty("db.username"),
+                        env.getProperty("db.password"));
+        Properties connProps = new Properties();
 
-        // TODO Parameterize driver name
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        // MySQL 8 default character set is UTF-8
+        connProps.setProperty("useUnicode", env.getProperty("db.useUnicode"));
+        connProps.setProperty("characterEncoding", env.getProperty("db.characterEncoding"));
+        dataSource.setConnectionProperties(connProps);
+
+        dataSource.setDriverClassName(env.getProperty("db.driverClassName"));
         return dataSource;
     }
 
